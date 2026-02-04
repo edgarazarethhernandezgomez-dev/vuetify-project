689e9c1416de991211d53204c3b2a1a4279ec7d7
Edgar Hernandez <edgarazarethhernandezgomez@gmail.com>
2026-02-04 00:03:32 -0600

    Agregada vista de Login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <HelloWorld />
-</template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Iniciar Sesión</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="Usuario o Correo"
+                prepend-icon="mdi-account"
+                type="text"
+              ></v-text-field>
 
-<script setup>
-  //
-</script>
+              <v-text-field
+                label="Contraseña"
+                prepend-icon="mdi-lock"
+                type="password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Entrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
